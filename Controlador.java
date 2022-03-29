ac6304a5ef558f52b45e8addfea4603a57c7ab32
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-03-28 22:26:14 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,2 +1,12 @@
 public class Controlador {
+    public boolean isInt(String value)
+    {
+        try {
+            Integer.parseInt(value);
+            return true;
+        }catch(Exception e)
+        {
+            return false;
+        }
+    }
 }
